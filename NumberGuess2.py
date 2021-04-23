63c973ac10e21644c8530dfa073339872cc54ed3
Shreyash Kashyap <76607993+SYK-08@users.noreply.github.com>
2021-04-23 19:39:56 +0530

    Create NumberGuess2.py

---
+++ NumberGuess2.py
@@ -0,0 +1,26 @@
+import random
+
+random_no = random.randint(1,11)
+tries = 0
+
+Name = input('Welcome, please enter your name:')
+welcome = input('Hey '+ Name + ' will you play a small game with me?(y/n)')
+
+if welcome=='n':
+    print('Oh okay, bye.')
+elif welcome=='y':
+    guess = input('Great! I am thinking of a number between 0-11 try to guess it:\n')
+    if guess==random_no:
+        print('-------------------------------------------')
+        print('Hurray!')
+        print('The number was ' + str(random_no)+ ' and you did it in '+ str(tries)+' tries')
+        print('-------------------------------------------')
+
+while guess!=random_no:
+    tries+=1
+    guess = int(input('Try again:'))
+    if guess==random_no:
+        print('-------------------------------------------')
+        print('Hurray!')
+        print('The number was ' + str(random_no)+ ' and you did it in '+ str(tries)+' tries')
+        print('-------------------------------------------')
