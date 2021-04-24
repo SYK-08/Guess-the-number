34a42a25b2e1678de65014d66303eef39172b677
Shreyash Kashyap <76607993+SYK-08@users.noreply.github.com>
2021-04-24 19:21:21 +0530

    Update NumberGuess2.py

--- NumberGuess2.py
+++ NumberGuess2.py
@@ -9,8 +9,12 @@ welcome = input('Hey '+ Name + ' will you play a small game with me?(y/n)')
 if welcome=='n':
     print('Oh okay, bye.')
 elif welcome=='y':
-    guess = input('Great! I am thinking of a number between 0-11 try to guess it:\n')
-    if guess==random_no:
+    guess = int(input('Great! I am thinking of a number between 0-11 try to guess it:\n'))
+    if guess>random_no:
+        print('Try a smaller number.')
+    elif guess<random_no:
+        print('Try a larger number.')
+    elif guess==random_no:
         print('-------------------------------------------')
         print('Hurray!')
         print('The number was ' + str(random_no)+ ' and you did it in '+ str(tries)+' tries')
@@ -19,7 +23,11 @@ elif welcome=='y':
 while guess!=random_no:
     tries+=1
     guess = int(input('Try again:'))
-    if guess==random_no:
+    if guess>random_no:
+        print('Try a smaller number.')
+    elif guess<random_no:
+        print('Try a larger number.')
+    elif guess==random_no:
         print('-------------------------------------------')
         print('Hurray!')
         print('The number was ' + str(random_no)+ ' and you did it in '+ str(tries)+' tries')
