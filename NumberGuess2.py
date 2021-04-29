89042d3358a3c79e735ae48f9229c147b3e93542
Shreyash Kashyap <76607993+SYK-08@users.noreply.github.com>
2021-04-29 16:18:14 +0530

    Update NumberGuess2.py

--- NumberGuess2.py
+++ NumberGuess2.py
@@ -1,34 +1,41 @@
 import random
+print("Welcome to the Number Guessing game.")
+def main():
+    run_game()
+    play_again()
 
-random_no = random.randint(1,10)
-tries = 0
+def run_game():
+    counter = 1
+    random_no = random.randint(1,10)
+    while counter > 0 and counter <=5:
+        guess = int(input("Try to guess the number:\n"))
+        if guess != random_no and guess > random_no:
+            print("Wrong number.Try a lower value.")
+            counter += 1
+        elif guess != random_no and guess < random_no:
+            print("Wrong number.Try a higher value.")
+            counter += 1
+        else:
+            print("Well done! The number was ", str(random_no), " and you did it in " ,str(counter), " attempts.")
+            
+            play_again()
+        
+        if counter == 2:
+            print("4 attempts left before the program ends.")
+        if counter == 3:
+            print("3 attempts left before the program ends.")
+        if counter == 4:
+            print("2 attempts left before the program ends.")
+        if counter == 5:
+            print("1 attempt left before the program ends.") 
 
-Name = input('Welcome, please enter your name:')
-welcome = input('Hey '+ Name + ' will you play a small game with me?(y/n)')
-
-if welcome=='n':
-    print('Oh okay, bye.')
-elif welcome=='y':
-    guess = int(input('Great! I am thinking of a number between 0-11 try to guess it:\n'))
-    if guess>random_no:
-        print('Try a smaller number.')
-    elif guess<random_no:
-        print('Try a larger number.')
-    elif guess==random_no:
-        print('-------------------------------------------')
-        print('Hurray!')
-        print('The number was ' + str(random_no)+ ' and you did it in '+ str(tries)+' tries')
-        print('-------------------------------------------')
-
-while guess!=random_no:
-    tries+=1
-    guess = int(input('Try again:'))
-    if guess>random_no:
-        print('Try a smaller number.')
-    elif guess<random_no:
-        print('Try a larger number.')
-    elif guess==random_no:
-        print('-------------------------------------------')
-        print('Hurray!')
-        print('The number was ' + str(random_no)+ ' and you did it in '+ str(tries)+' tries')
-        print('-------------------------------------------')
+def play_again():
+    while True:
+        Q = input('Would you like to play again?(y/n):')
+        if Q == 'y':
+            main()
+        if Q == 'n':
+            exit()       
+        else:
+            print("Invalid input")
+main()
